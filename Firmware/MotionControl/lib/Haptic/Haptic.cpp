e75b1259a0cc4e130e084175f4980a134eccb84a
FreeFa11 <rabinrai24816@gmail.com>
2024-12-18 00:05:55 +0545

    Improved Haptic

--- Firmware/MotionControl/lib/Haptic/Haptic.cpp
+++ Firmware/MotionControl/lib/Haptic/Haptic.cpp
@@ -1,14 +1,72 @@
 #include <Haptic.h>
 
 
-void SetupHaptic()
+Haptic::Haptic()
+{}
+Haptic::~Haptic()
+{}
+
+void Haptic::Initialize(uint8_t OutputPin, uint8_t Channel, uint8_t Resolution, uint32_t Frequency)
 {
-    ledcSetup(PWMChannel, PWMFrequency, PWMResolution);
-    ledcAttachPin(OutputPin, PWMChannel);
+    this->Pin = OutputPin;
+    this->Channel = Channel;
+    
+    ledcSetup(Channel, Frequency, Resolution);
+    ledcAttachPin(OutputPin, Channel);
+
 }
-void WriteHaptic(uint16_t Intensity, uint16_t Period)
+
+void Haptic::Write(uint16_t Intensity, uint16_t Period)
 {
-    ledcWrite(PWMChannel, Intensity);
+    ledcWrite(Channel, Intensity);
     vTaskDelay(Period / portTICK_RATE_MS);
-    ledcWrite(0, PWMChannel);
+    ledcWrite(Channel, 0);
+}
+
+void Haptic::Write(int16_t StartIntensity, int16_t EndIntensity, int16_t Period)
+{
+    int16_t Periods = Period / 5;
+    int16_t Increment = (EndIntensity - StartIntensity) / Periods;
+
+    for (int16_t i=0; i<Periods; ++i)
+    {
+        ledcWrite(Channel, (StartIntensity + (i*Increment)));
+        vTaskDelay(5 / portTICK_RATE_MS);
+    }
+
+    ledcWrite(Channel, EndIntensity);
+
+}
+
+
+void Haptic::Vibrate(Pattern VibrationPattern)
+{
+    switch (VibrationPattern)
+    {
+        case Pattern::Subtle:
+            this->Write(HapticIntensityHigh, 20);
+            break;
+
+        case Pattern::Firm:
+            this->Write(0, HapticIntensityHigh, 10);
+            this->Write(HapticIntensityHigh, 50);
+            this->Write(HapticIntensityHigh, 0, 10);
+            break;
+
+        case Pattern::Long:
+            this->Write(0, HapticIntensityMedium, 50);
+            this->Write(HapticIntensityMedium, 200);
+            this->Write(HapticIntensityMedium, 0, 80);
+            break;
+            
+        case Pattern::Double:
+            this->Write(0, HapticIntensityMedium, 15);
+            this->Write(HapticIntensityHigh, 25);
+            this->Write(0, 65);
+            this->Write(HapticIntensityMedium, 30);
+            break;
+        
+        default:
+            break;
+    }
 }
--- Firmware/MotionControl/lib/Haptic/Haptic.h
+++ Firmware/MotionControl/lib/Haptic/Haptic.h
@@ -3,12 +3,32 @@
 
 
 // Definitions
-#define OutputPin           4
+#define HapticOutputPinDefault    4
+#define HapticChannelDefault      0
+#define HapticResolutionDefault   8
+#define HapticFrequencyDefault    30000
 
-#define PWMChannel          0
-#define PWMFrequency        30000
-#define PWMResolution       8
+#define HapticIntensityLow        100
+#define HapticIntensityMedium     150
+#define HapticIntensityHigh       220
 
-// Functions
-void SetupHaptic();
-void WriteHaptic(uint16_t Intensity, uint16_t Period);
+// Class
+enum Pattern {Subtle, Firm, Long, Double};
+
+class Haptic
+{
+private:
+    uint8_t Pin, Channel;
+    void Write(uint16_t Intensity, uint16_t Period);
+    void Write(int16_t StartIntensity, int16_t EndIntensity, int16_t Period);
+
+public:
+    Haptic();
+    ~Haptic();
+
+    // The Haptic is controlled using PWM for fine control over the intensity of Vibration
+    void Initialize(uint8_t OutputPin = HapticOutputPinDefault, uint8_t Channel = HapticChannelDefault, uint8_t Resolution = HapticResolutionDefault, uint32_t Frequency = HapticFrequencyDefault);
+
+    // Haptic class supports vibrations corresponding to values of "Pattern" enum
+    void Vibrate(Pattern VibrationPattern);
+};
